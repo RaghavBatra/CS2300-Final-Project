eb8ca269e0ebd8a1cfe91ffccdb62b4583a6fec6
Ryan Yoon <Ryan Yoon>
2017-05-01 22:12:16 -0400

    welcome section implemented

--- index.php
+++ index.php
@@ -21,10 +21,23 @@
 
     <?php include 'php/navbar.php' ?>
 
-    <div id='banner_wrapper'>
+    <div id='banner_wrapper' class='section_wrapper'>
+        <!-- Image adapted from http://www.icfsne.com/2016/home -->
         <img src='images/banner.jpg' alt='banner'>
     </div>
 
+    <div id='welcome_wrapper' class='section_wrapper'>
+        <h2>Welcome to the</h2>
+        <h1>Cornell Food Science Club</h1>
+        <p>We do more than just eat food!</p>
+        <div id='scroll_button'>
+            <a href="#about_wrapper">Learn more about us!</a>
+        </div>
+    </div>
+
+    <div id='about_wrapper' class='section_wrapper'>
+        
+    </div>
 
 </body>
 </html>

64dd9423e17b9643e8fad1db74cec28d4ec182e5
Spencer Weiss <spencerc.weiss@gmail.com>
2017-05-03 18:04:57 -0400

    contact page update

--- contact.php
+++ contact.php
@@ -13,8 +13,47 @@
         <h2>Contact form to be implemented!</h2>
     </div>
 
-    <!--
-        /* div containing the contact form, which allows users to email the club */
+
+        
+        
+	<div id='welcome_wrapper' class='section_wrapper'>
+        <?php
+			$action=$_REQUEST['action'];
+			if ($action=="")    /* display the contact form */
+   				{
+    	?>
+			    <form  action="" method="POST" enctype="multipart/form-data">
+			    <input type="hidden" name="action" value="submit">
+			    Your name:<br>
+			    <input name="name" type="text" value="" size="30"/><br>
+			    Your email:<br>
+			    <input name="email" type="text" value="" size="30"/><br>
+			    Your message:<br>
+			    <textarea name="message" rows="7" cols="30"></textarea><br>
+			    <input type="submit" value="Send email"/>
+			    </form>
+		<?php
+			    } 
+			else                /* send the submitted data */
+    			{
+			    $name=$_REQUEST['name'];
+			    $email=$_REQUEST['email'];
+			    $message=$_REQUEST['message'];
+			    if (($name=="")||($email=="")||($message==""))
+			        {
+					echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+				    }
+			    else {
+				    $from="From: $name<$email>\r\nReturn-path: $email";
+			        $subject="Message sent using your contact form";
+					// mail("youremail@yoursite.com", $subject, $message, $from);
+					echo "Email sent!";
+			    }
+		    }  
+		?>
+	</div>
+	<!--
+		/* div containing the contact form, which allows users to email the club */
         email div:
             form (post): 
                 input (text box) - name
@@ -23,6 +62,7 @@
                 input (submit) - send email
         
         /* php code to send email */
+	
         if submit = true then
             validate inputs (name and netid)
             php send email function to club email

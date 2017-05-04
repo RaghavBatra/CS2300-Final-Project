3bac976361945a2c3c066b02c82d9f91b5173840
Raghav Batra <rb698@cornell.edu>
2017-05-04 18:31:11 -0400

    Major changes to photos.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <?php 
         require_once 'config.php';
         include 'includes/navbar.php';
-        $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME); // connect with the mysql database
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); // connect with the mysql database
         if($mysqli->connect_error) { // terminate script if fail to connect with database
             die("Connection failed: " . $mysqli->connect_error);
         }
--- photos.php
+++ photos.php
@@ -5,7 +5,7 @@
     <?php 
         require_once 'config.php';
         include 'includes/navbar.php';
-        // $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME); 
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
     ?>
 
     <div id='banner_wrapper' class='section_wrapper'>
@@ -14,17 +14,13 @@
     </div>
 
     <div id='welcome_wrapper' class='section_wrapper'>
-        <h2>Photo gallery to be implemented!</h2>
+        <h2>Photo gallery</h2>
     </div>
 
     <!--
         /* Search form over multiple fields similar to P3 */
         form (post):
-            input (text box) - image name
-            input (text box) - album name
-            input (text box) - description
-            input (date) - date
-            input (submit) - search
+
 
         /* php code that querys the database for images/albums that match the search parameters */
         if submit = true then
@@ -42,6 +38,76 @@
                 Clicking album generates custom page that displays images in album same as P3
                 php code needed to generate table (copy from P3)
     -->
+
+    <?php
+    
+    if (!isset($_POST['search'])) {
+        echo "<form action='' method='post' id='search'>
+                    <input type='text' name='search' placeholder ='Search'>
+                    <input type='submit' name='submit' value = 'Submit'>    
+        </form>";
+        
+        // If nothing has been selected, display all albums
+        $query = "SELECT * FROM albums";
+        $albums = $mysqli->query($query);
+        echo "<div id = 'search_grid'>";
+        if ($albums->num_rows > 0) {
+            while ($row = $albums->fetch_assoc()) {
+                echo "<div class = 'search_block'>";
+                echo "<p> <b> Album name: " . $row["title"] . "</b> </p>";
+                echo "<p> <b> Album credits: " . $row["coverImageFilePath"] . "</b> </p>";
+                if (empty($row['coverImageFilePath'])) {
+                    $imageFilePath = 'album_placeholder.png';
+                }
+                else {
+                    $imageFilePath =  $row["coverImageFilePath"];
+                }
+                echo "<img src = 'images/$imageFilePath' alt = 'images/$imageFilePath' class = 'search_display'> ";
+                echo "</div>";    
+            }
+            echo "</div>";
+        }
+    }
+    
+    else {
+        $query = "SELECT * FROM albums WHERE title REGEXP ? ";
+        
+        $stmt = $mysqli->stmt_init();
+        
+        if ($stmt->prepare($query)) {
+            $stmt->bind_param('s', $searchInput);
+            $searchInput = htmlentities(filter_input(INPUT_POST, 'search', FILTER_SANITIZE_STRING));
+            $stmt->execute();
+            $stmt->store_result();
+        }
+        
+        if ($stmt->num_rows > 0) { 
+            echo "<div id = 'search_grid'>";
+            while($row = $stmt->fetch()) { 
+            
+                echo "<div class = 'search_block'>";
+                echo "<p> <b> Album name: " . $row["title"] . "</b> </p>";
+                echo "<p> <b> Album credits: " . $row["coverImageFilePath"] . "</b> </p>";
+                if (empty($row['coverImageFilePath'])) {
+                    $imageFilePath = 'album_placeholder.png';
+                }
+                else {
+                    $imageFilePath =  $row["coverImageFilePath"];
+                }
+                echo "<img src = 'images/$imageFilePath' alt = 'images/$imageFilePath' class = 'search_display'> ";
+                echo "</div>";    
+            }
+        echo "</div>";        
+        }
+        
+        else {
+            echo "No results!";
+        }
+    }
+        
+    
+    $mysqli->close();
+    ?>
 
 </body>
 </html>

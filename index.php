58f836ea34230f42026324acb1679f2db049e9f4
Spencer Weiss <spencerc.weiss@gmail.com>
2017-05-05 15:20:53 -0400

    food sicence

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         <h2>We are a community of Cornell University students who are 
         passionate about anything related to food! Our <span>Vision</span> is to
         <span>Support</span> students interested in the food industry, food 
-        sicence, and/or the culinary arts, <span>Promote</span> club membership 
+        science, and/or the culinary arts, <span>Promote</span> club membership 
         across various majors, and <span>Advocate</span> opportunities in the 
         food industry to students across campus.</h2>
     </div>

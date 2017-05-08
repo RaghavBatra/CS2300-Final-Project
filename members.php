a057ed9dc7144c1bcddc06b4401018c47ee043a1
Ryan Yoon <Ryan Yoon>
2017-05-08 19:58:05 -0400

    members page implemented

--- members.php
+++ members.php
@@ -13,23 +13,61 @@
         <img src='images/banner.jpg' alt='banner'>
     </div>
 
-    <div id='welcome_wrapper' class='section_wrapper'>
-        <h2>Profiles of club executives and members to be implemented!</h2>
+    <div id="executives_wrapper" class='section_wrapper'>
+        <h1>Executive Board</h1>
+        <div class='table_row'>
+            <div class='profile_wrapper'>
+                <img src='images/president.jpg' alt='Halle Bershad'>
+                <h2>Halle Bershad</h2>
+                <h3>President</h3>
+                <h4>Class of 2017</h4>
+            </div>
+            <div class='profile_wrapper'>
+                <img src='images/vice_president.jpg' alt='Nayara Luna'>
+                <h2>Nayara Luna</h2>
+                <h3>Vice President</h3>
+                <h4>Class of 2019</h4>
+            </div>
+            <div class='profile_wrapper'>
+                <img src='images/secretary.jpg' alt='Sierra Jamir'>
+                <h2>Sierra Jamir</h2>
+                <h3>Secretary</h3>
+                <h4>Class of 2018</h4>
+            </div>
+        </div>
+        <div class='table_row'>
+            <div class='profile_wrapper'>
+                <img src='images/p4.jpg' alt='Sean Dolan'>
+                <h2>Sean Dolan</h2>
+                <h3>Treasurer</h3>
+                <h4>Class of 2019</h4>
+            </div>
+            <div class='profile_wrapper'>
+                <img src='images/p5.jpg' alt='Kaitlin Steinleitner'>
+                <h2>Kaitlin Steinleitner</h2>
+                <h3>Transfer Representative</h3>
+                <h4>Class of 2019</h4>
+            </div>
+            <div class='profile_wrapper'>
+                <img src='images/p6.jpg' alt='Morgan Dickens'>
+                <h2>Morgan Dickens</h2>
+                <h3>Community Outreach</h3>
+                <h4>Class of 2020</h4>
+            </div>
+        </div>
+        <div class='table_row'>
+            <div class='profile_wrapper'>
+                <img src='images/p7.jpg' alt='Morgan Smith'>
+                <h2>Morgan Smith</h2>
+                <h3>Community Outreach</h3>
+                <h4>Class of 2020</h4>
+            </div>
+        </div>
     </div>
 
-    <!--
-        /* php code needed to generate table view of club members */
-            connect to database
-            write prepared statement to fetch entries in profiles table
-            execute prepared statement
-            loop through the result of statement execution and ouput html table 
-                nicely formated to display an image of each member along with 
-                a brief description
-
-        /* jQuery script for onclick events */
-            if profile picture is clicked then
-                enlarge picture and dim rest of page
-    -->
+    <div id='members_wrapper' class='section_wrapper'>
+        <h1>Club Members</h1>
+    </div>
 
 </body>
 </html>

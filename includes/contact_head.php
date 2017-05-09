7d53b71a2997f02116d6886f8b4bdd6ca03bbc2c
Spencer Weiss <spencerc.weiss@gmail.com>
2017-05-09 10:53:31 -0400

    formatting, fixed JS

--- includes/contact_head.php
+++ includes/contact_head.php
@@ -3,15 +3,11 @@
  * Contains the html doctype declaration and head tag with meta data about the 
  * website */
 session_start();
-
 echo "<!doctype html>
-
 <html>
 <head>
     <meta charset='utf-8'>
-
     <title>Cornell Food Science Club</title>
-
     <meta name='description' content='Cornell Food Science Club'>
     <meta name='keywords' content='Cornell, Food, Science, Club'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
@@ -20,29 +16,28 @@ echo "<!doctype html>
     <script src='js/main.js'></script>
     <script src='js/lightbox-plus-jquery.js'></script>
     <script src='js/lightbox.js'></script>
-    <script src='./includes/jquery.js'></script>
-    <script src='./includes/jquery-1.11.1.js'></script>
-	<script src='./includes/jquery.validate.js'></script>
-
+    <script src='js/jquery.js'></script>
+    <script src='js/jquery-1.11.1.js'></script>
+	<script src='js/jquery.validate.js'></script>
+	
     <link rel='stylesheet' type='text/css' href='css/styles.css'>
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway'>
     <link rel='stylesheet' href='css/lightbox.css'>
     <link rel='stylesheet' href='css/screen.css'>
-
     
     <script>
+    
 	$.validator.setDefaults({
 		submitHandler: function() {
-			alert('Thanks for reaching out! We'll get back to you as soon as possible.');
+			alert('Thanks for reaching out! We\'ll get back to you as soon as possible.');
 			document.commentForm.submit();
 			location.reload();
 		}
 	});
-
 	$().ready(function() {
 		// validate the comment form when it is submitted
 		$('#commentForm').validate();
-	});
+	})	
 	</script>
 </head>"
 ?>

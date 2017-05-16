ed42de12929ee8ef1380b118b6461793227f325b
Raghav Batra <rb698@cornell.edu>
2017-05-16 15:09:50 -0400

    Added not found redirect page

---
+++ not_found.php
@@ -0,0 +1,21 @@
+<?php include 'includes/head.php' ?>
+
+<body>
+
+    <?php 
+        require_once 'config.php';
+        include 'includes/navbar.php';
+        include 'includes/setup.php'; 
+    ?>
+
+    <div id='banner_wrapper' class='section_wrapper'>
+        <!-- Image adapted from http://www.icfsne.com/2016/home -->
+        <img src='images/banner.jpg' alt='banner'>
+    </div>
+
+    <div id='dummy_wrapper' class='section_wrapper'>
+        <h2>Sorry, this album does not exist</h2>
+    </div>
+    
+    </body>
+</html>
